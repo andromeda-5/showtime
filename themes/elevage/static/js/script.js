d620f467c2b2e58dbc5f033233f8af69b6ed8e5f
leirion <lily.aliyeva@mail.utoronto.ca>
2020-06-23 12:22:34 +0200

    began responsive design, responsive menu for mobile

--- themes/elevage/static/js/script.js
+++ themes/elevage/static/js/script.js
@@ -1,4 +1,10 @@
-$(document).ready(function(){
-    
-});
+function myFunction() {
+    console.log('triggered');
+    var x = document.getElementById("top-menu");
+    if (x.className == "menu-div") {
+        x.className += " responsive";
+    } else {
+        x.className = "menu-div";
+    }
+}
 
