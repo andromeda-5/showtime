1b4d15b10cc13f007fce9f69a8c56e3ef06ed34f
leirion <lily.aliyeva@mail.utoronto.ca>
2020-07-01 13:43:17 +0200

    added dropdown menu to nav items for each individual horse

--- themes/elevage/static/js/script.js
+++ themes/elevage/static/js/script.js
@@ -1,10 +1,1 @@
-function myFunction() {
-    console.log('triggered');
-    var x = document.getElementById("top-menu");
-    if (x.className == "menu-div") {
-        x.className += " responsive";
-    } else {
-        x.className = "menu-div";
-    }
-}
-
+// import 'bootstrap/dist/js/bootstrap.bundle';
